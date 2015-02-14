f2dc486d0f832d4bfb1a436fc807beac06c46da2
cosmointeractive <cosmo@cimwebdesigns.com>
2015-02-14 10:43:58 -0500

    Adding documentation to the Main controller

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -16,8 +16,13 @@
  
 // ------------------------------------------------------------------------
 
-class Main extends Controller
+/**
+ * This class in essence functions as a router class
+ * Forward page request to the appropriate module/controlelr 
+ */
+class Main extends Public_Controller
 {	
+
 	public function index()
 	{        
 		// $bc = new Breadcrumb();
@@ -26,10 +31,74 @@ class Main extends Controller
 		// $template = $this->loadView('main_view');
         // $template->set('bread', $bc->makeBread());
 		// $template->render();
-        $this->loadModule('site/pages');
-        $pages = new Pages();
+        // $this->loadModule('site/pages');
+        // $pages = new Pages();
         
+        $this->loadContent(Url::segment(0));
 	}
+    
+    /**
+     * Load the appropriate controller based on the url slug
+     *
+     * @access      public 
+     * @var         string $segment The first segment if the url minus the base_path
+     */
+    public function loadContent($segment)
+    {
+        $pageController = array();
+        
+        // @note    Load a list of pages from the database and save them in an array
+        // @note    Consider populating $pageController array with data from a database
+        switch($segment) {
+            case $this->pageExists($segment) : $pageController = array(
+                'module' => 'site',
+                'controller' => 'pages',
+                'method' => 'index'
+            );
+            break;
+            
+            case 'contact' : $pageController = array(
+                'module' => 'site',
+                'controller' => 'contact',
+                'method' => 'index'
+            );
+            break;
+            
+            case 'blog' : $pageController = array(
+                'module' => 'site',
+                'controller' => 'blog',
+                'method' => 'index'
+            );
+            break;
+        }
+        // This will error to the 404 page if the module/controller is not found
+        $this->loadModule($pageController['module'] . '/' . $pageController['controller']);
+        $object = ucfirst($pageController['controller']);
+        $pages = new $object;
+        
+    }
+    
+    /**
+     * Get a list of active pages from the database
+     * @return      string $pageSlug The url slug of the page
+     */
+    private function pageExists($pageSlug) 
+    {
+        if(in_array($pageSlug, array(
+            '',
+            'home', 
+            'about',
+            'mission',
+            'giving', 
+            'contact',
+            'blog'
+        ))) {
+            return $pageSlug;
+        } else {
+            return false;
+        }
+        
+    }
     
 }
 
